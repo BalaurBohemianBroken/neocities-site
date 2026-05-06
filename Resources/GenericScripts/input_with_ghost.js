eeca2d3ca36c285e9087080d33609b562aece9a5
BalaurBohemianBroken <realvulpes@gmail.com>
2026-05-06 10:34:32 +0100

    Improved input handling, again

--- Resources/GenericScripts/input_with_ghost.js
+++ Resources/GenericScripts/input_with_ghost.js
@@ -1,17 +1,14 @@
-function oninput_ghost(input_element, ghost_id, ghost_default, limit_size = true, regex_remove = null) {
+function oninput_ghost(input_element, ghost_id, ghost_default) {
     let default_len = ghost_default.length;
     let ghost = document.getElementById(ghost_id);
-    if (regex_remove !== null) {
-        input_element.value = input_element.value.replace(regex_remove, "");
-    }
     let input_len = input_element.value.length;
     let ghost_size = Math.max(ghost_default.length - input_len, 0);
     ghost.innerText = ghost_default.slice(0, ghost_size);
+}
 
-    if (!limit_size) {
-        return;
-    }
-    if (input_len > ghost_default.length) {
-        input_element.value = input_element.value.slice(0, ghost_default.length);
-    }
+function oninput_limit(input_element, size_limit=0, regex_remove=null) {
+    if (regex_remove !== null)
+        input_element.value = input_element.value.replace(regex_remove, "");
+    if (size_limit !== 0)
+        input_element.value = input_element.value.slice(0, Math.min(input_element.value.length, size_limit));
 }
